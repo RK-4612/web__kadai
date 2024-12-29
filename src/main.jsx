6cb6bafad84c4dba5b16fcb7d805c86465bc266a
RK-4612 <k.riku1216@au.com>
2024-12-29 23:13:24 +0900

    go

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-createRoot(document.querySelector("body")).render(<App />);
+createRoot(document.querySelector("#content")).render(<App />);
